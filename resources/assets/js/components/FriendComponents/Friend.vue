ece57e3b76fa952f8e5c56ff323d5b281a30ec50
Jasper Martin <jaspervanbrianmartin@gmail.com>
2018-08-19 23:16:19 +0800

    Change push to set to add properies in array, and delete to remove properites in array.

--- resources/assets/js/components/FriendComponents/Friend.vue
+++ resources/assets/js/components/FriendComponents/Friend.vue
@@ -1,15 +1,15 @@
 <template>
 	<div class="list-group">
 		<a href="#!" v-for="friend in friends" class="list-group-item list-group-item-action flex-column align-items-start">
-			<div class="d-flex w-100 justify-content-between" v-if="editIndexes.includes(friend.id)">
-				<input type="text" v-model="friend.name" class="form-control">
-				<button class="btn btn-info" @click="updateFriend(friend.id, friend.name)">Save</button>
+			<div class="d-flex w-100 justify-content-between" v-if="editing.hasOwnProperty(friend.id)">
+				<input type="text" v-model="editing[friend.id]" class="form-control">
+				<button class="btn btn-info" @click="updateFriend(friend.id, editing[friend.id])">Save</button>
 				<button class="btn btn-danger" @click="cancelEdit(friend.id)">Cancel</button>
 			</div>
 			<div class="d-flex w-100 justify-content-between" v-else>
 				<h5 class="mb-1" v-text="friend.name"></h5>
 				<div>
-					<button class="btn btn-outline-info" @click="editFriend(friend.id)">Edit</button>
+					<button class="btn btn-outline-info" @click="editFriend(friend.id, friend.name)">Edit</button>
 					<button class="btn btn-outline-danger" @click="deleteFriend(friend.id)">Remove</button>
 				</div>
 			</div>
@@ -28,29 +28,22 @@
 		},
 		data() {
 			return {
-				newName: "",
-				editIndexes: [],
+				editing: [],
 			}
 		},
 		methods: {
-			editFriend(id) {
-				this.editIndexes.push(id);
+			editFriend(id, name) {
+				this.$set(this.editing, id, name);
 			},
 			cancelEdit(id) {
-				let index = this.editIndexes.indexOf(id);
-				if (index > -1) {
-					this.editIndexes.splice(index, 1);
-				}
+				this.$delete(this.editing, id);
 			},
 			updateFriend(id, name) {
 				axios.post(`http://127.0.0.1:8000/friends/${id}/update`, {
 					name,
 				})
 				.then((response) => {
-					let index = this.editIndexes.indexOf(id);
-					if (index > -1) {
-						this.editIndexes.splice(index, 1);
-					}
+					this.$delete(this.editing, id);
 					this.$emit("done");
 				})
 				.catch((response) => {
