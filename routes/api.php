60ba6793a540cc739ab2a8d602fdbe1bb8f97a92
Jasper Martin <jaspervanbrianmartin@gmail.com>
2018-08-23 23:15:25 +0800

    Group api routes in namespace instead of hardcode.

--- routes/api.php
+++ routes/api.php
@@ -17,6 +17,8 @@ Route::middleware('auth:api')->get('/user', function (Request $request) {
     return $request->user();
 });
 
-Route::apiResources([
-	'friends' => 'API\FriendController'
-]);
+Route::group(['namespace' => 'API'], function() {
+	Route::apiResources([
+		'friends' => 'FriendController'
+	]);
+});
